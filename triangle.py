6f91e22c18864afa8594b53f1e8118bb2ac04e9c
myavrum <myavrum@ysu.am>
2019-09-23 20:54:07 +0400

    How to create a triangle

---
+++ triangle.py
@@ -0,0 +1,5 @@
+leng1 = int(input("Pls, enter the length of the first leng by a right triangle! ->"))
+leng2 = int(input("Pls, enter the length of the second leng by a right triangle! ->"))
+hypotenuse = int(input("Pls, enter the length of the hypotenuse by a right triangle! ->"))
+right_triangle = "It is a right triangle? "
+print(right_triangle + str(leng1 + leng2 < hypotenuse))
