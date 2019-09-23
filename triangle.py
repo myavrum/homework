9d0eb1c51c346f81941d3f5f29f8032f7309a7b7
myavrum <myavrum@ysu.am>
2019-09-23 20:58:21 +0400

    a small correction

--- triangle.py
+++ triangle.py
@@ -2,4 +2,4 @@ leng1 = int(input("Pls, enter the length of the first leng by a right triangle! 
 leng2 = int(input("Pls, enter the length of the second leng by a right triangle! ->"))
 hypotenuse = int(input("Pls, enter the length of the hypotenuse by a right triangle! ->"))
 right_triangle = "It is a right triangle? "
-print(right_triangle + str(leng1 + leng2 < hypotenuse))
+print(right_triangle + str(leng1 + leng2 > hypotenuse))
