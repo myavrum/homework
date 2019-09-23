074ef9e369ad4c0f4cc38a4caedf0db34615b196
myavrum <myavrum@ysu.am>
2019-09-23 20:12:49 +0400

    4 new lines in

---
+++ old_man.py
@@ -0,0 +1,4 @@
+your_name = input("Pls, enter your name! ->")
+your_age = input("Pls, enter your borning date! ->")
+current_date = 2019 - int(your_age)
+print("Hi, " + str(your_name) + ", you will be 100 years old in " + str(current_date) + " years")
