7566ea4a50a085eb8ba79b1b7ff6da758512e208
Marat Yavrumyan <myavrum@ysu.am>
2019-10-14 19:25:36 +0400

    a new file

---
+++ dict.py
@@ -0,0 +1,9 @@
+#Dictionary
+#key-value pairs
+#my_dictionary = {"key": "value"}
+
+human = {"name": "Marat", "surname": "Yavrumyan", "age": 19, "is_married": True}
+print(human)
+
+#print as a list
+print(human["name"], human["age"], human["is_married"])
