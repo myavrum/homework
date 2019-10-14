dc45e501c878566f738d4fb2fe2d760666211b99
Marat Yavrumyan <myavrum@ysu.am>
2019-10-14 19:33:13 +0400

    dictionary

--- dict.py
+++ dict.py
@@ -2,7 +2,18 @@
 #key-value pairs
 #my_dictionary = {"key": "value"}
 
-human = {"name": "Marat", "surname": "Yavrumyan", "age": 19, "is_married": True}
+human = {"name": "Marat", "surname": "Yavrumyan", "age": 40, "is_married": True}
+print(human)
+
+#change a value
+human["age"] = 30
+
+#add a new value
+human["have_pet"] = True
+
+#delete a value
+del human["surname"]
+
 print(human)
 
 #print as a list
